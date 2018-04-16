1c565f3b33c26d785f6738e6aa08f9b80b4de867
Parul Baweja <parulbaweja8@gmail.com>
2018-04-15 18:29:34 -0700

    Adjusted linear probing for quadtric as well, learned about formula for power of 2 table

--- linear_probing.py
+++ linear_probing.py
@@ -9,10 +9,11 @@ class HashElement(object):
 
 
 class HashTable(object):
-    def __init__(self):
+    def __init__(self, probe=1):
         self.arr = [None for i in xrange(16)]
         self.m = 0
         self.n = len(self.arr)
+        self.probe = probe
 
     def _hash_key(self, key):
         hashed = hashlib.md5(key)
@@ -24,27 +25,32 @@ class HashTable(object):
 
         elem = HashElement(key, value)
         bucket = self._hash_key(elem.key)
+        cur = bucket
+        base = 1
 
         while True:
-            if self.arr[bucket] is None or self.arr[bucket].tombstone:
-                self.arr[bucket] = elem
+            if self.arr[cur] is None or self.arr[cur].tombstone:
+                self.arr[cur] = elem
                 self.m += 1
                 return
             # increment bucket
-            bucket = (bucket + 1) % self.n
-            # quadratic - i += 1, bucket = (bucket + i ** 2) % self.n
+            cur = int(bucket + base / 2.0 + (base ** self.probe) / 2.0) % self.n
+            base += 1
 
     def lookup(self, key):
         bucket = self._hash_key(key)
+        cur = bucket
+        base = 1
 
-        # then check all buckets after, until we come back to the original bucket
         while True:
-            if self.arr[bucket] is None:
+            if self.arr[cur] is None:
                 raise KeyError(key + ' is not in the table')
-            if self.arr[bucket].key == key and not self.arr[bucket].tombstone:
-                return self.arr[bucket].val
-            bucket = (bucket + 1) % self.n
-            # quadratic - i += 1, bucket = (bucket + i ** 2) % self.n
+            if self.arr[cur].key == key and not self.arr[cur].tombstone:
+                return self.arr[cur].val
+            cur = int(bucket + base / 2.0 + (base ** self.probe) / 2.0) % self.n
+            # naive implementation causes loop shown in note below
+            # cur = (bucket + base ** self.probe) % self.n
+            base += 1
 
     def get(self, key, default=None):
         try:
@@ -54,15 +60,17 @@ class HashTable(object):
 
     def delete(self, key):
         bucket = self._hash_key(key)
+        cur = bucket
+        base = 1
 
         while True:
-            if self.arr[bucket] is None:
+            if self.arr[cur] is None:
                 return
-            if self.arr[bucket].key == key and not self.arr[bucket].tombstone:
-                self.arr[bucket].tombstone = True
+            if self.arr[cur].key == key and not self.arr[cur].tombstone:
+                self.arr[cur].tombstone = True
                 self.m -= 1
-            bucket = (bucket + 1) % self.n
-            # quadratic - i += 1, bucket = (bucket + i ** 2) % self.n
+            cur = int(bucket + base / 2.0 + (base ** self.probe) / 2.0) % self.n
+            base += 1
 
     def check_load(self):
         load_factor = self.m / float(self.n)
@@ -83,23 +91,24 @@ if __name__ == '__main__':
     import random
     import string
 
-    table = HashTable()
-    lst = []
-    chars = string.ascii_letters + string.digits
+    for probe in (1, 2, 3):
+        table = HashTable(probe)
+        lst = []
+        chars = string.ascii_letters + string.digits
 
-    for i in xrange(1000):
-        key = ''.join(random.sample(chars, 3))
-        value = ''.join(random.sample(chars, 3))
-        table.insert(key, value)
-        lst.append(key)
+        for i in xrange(1000):
+            key = ''.join(random.sample(chars, 3))
+            value = ''.join(random.sample(chars, 3))
+            table.insert(key, value)
+            lst.append(key)
 
-    for key in lst:
-        table.lookup(key)
+        for key in lst:
+            table.lookup(key)
 
-    key = ''.join(random.sample(chars, 4))
-    assert table.get(key) is None
-
-    for key in lst:
-        table.delete(key)
+        key = ''.join(random.sample(chars, 4))
         assert table.get(key) is None
+
+        for key in lst:
+            table.delete(key)
+            assert table.get(key) is None
 
